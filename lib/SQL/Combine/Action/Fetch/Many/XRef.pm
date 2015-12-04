b6c1c08d4a88cd57f3178483cba1237f275bccba
Stevan Little <stevan.little@booking.com>
2015-12-04 20:57:40 +0100

    some notes about the `xref` and how it relates to the `query`

--- lib/SQL/Combine/Action/Fetch/Many/XRef.pm
+++ lib/SQL/Combine/Action/Fetch/Many/XRef.pm
@@ -26,4 +26,23 @@ __END__
 
 =pod
 
+=head1 ATTRIBUTES
+
+=head2 C<query>
+
+This is inherited from the L<SQL::Combine::Action::Fetch> role
+but here it is expected to always be a B<CodeRef>.
+
+=head2 C<xref>
+
+This is an object which does the L<SQL::Combine::Action::Fetch>
+role, it is expected to return results in a form that matches
+what the C<query> in expecting.
+
+B<NOTE:> This attribute's type is L<SQL::Combine::Action::Fetch>
+specifically to allow for it to be either a C<Fetch::One> or a
+C<Fetch::Many> depending on what the C<query> is expecting. Since
+both of these things are under the control of the user of this
+class it makes sense to defer proper usage to them.
+
 =cut
